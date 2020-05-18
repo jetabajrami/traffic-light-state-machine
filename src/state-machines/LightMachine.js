ab7057fc3cfad90bf7b6d9fbfe3dfc398cd4295f
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.home>
2020-05-17 20:14:36 -0400

    set up LightMachine

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -1,9 +1,26 @@
 import { createMachine, interpret } from 'xstate';
 
 const lightMachine = createMachine({
-  // Your state machine blueprint here
-
-});
+  id: "traficLight",
+  initial: "red",
+  states: {
+    red: {
+      on: {
+        changeLight: "green",
+      }
+    },
+    green: {
+      on: {
+        changeLight: "yellow",
+      }
+    },
+    yellow: {
+      on: {
+        changeLight: "red",
+      }
+    },
+  }
+}); 
 
 
 const service = interpret(lightMachine);
